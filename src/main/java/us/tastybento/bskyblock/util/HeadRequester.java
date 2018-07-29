fbdd39e53b7d9a1381d2dccd37f9fd96f7ab2d22
tastybento <tastybento@wasteofplastic.com>
2018-07-29 13:42:25 -0700

    Initial commit - copy over from BSkyBlock.

---
+++ src/main/java/us/tastybento/bskyblock/util/HeadRequester.java
@@ -0,0 +1,11 @@
+package us.tastybento.bskyblock.util;
+
+import us.tastybento.bskyblock.api.panels.PanelItem;
+
+public interface HeadRequester {
+    
+    /**
+     * @param item - panel item
+     */
+    void setHead(PanelItem item);
+}
