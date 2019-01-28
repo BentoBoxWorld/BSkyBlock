b83f831361bd642dbe91d1cbfb8adf10c60321e6
tastybento <tastybento@wasteofplastic.com>
2019-01-27 17:02:48 -0800

    Updated copyright year

--- src/main/java/world/bentobox/bskyblock/commands/IslandAboutCommand.java
+++ src/main/java/world/bentobox/bskyblock/commands/IslandAboutCommand.java
@@ -23,7 +23,7 @@ public class IslandAboutCommand extends CompositeCommand {
     @Override
     public boolean execute(User user, String label, List<String> args) {
         user.sendRawMessage("About " + getAddon().getDescription().getName() + " " + getAddon().getDescription().getVersion() + ":");
-        user.sendRawMessage("Copyright (c) 2017 - 2018 tastybento, Poslovitch");
+        user.sendRawMessage("Copyright (c) 2017 - 2019 tastybento, Poslovitch");
         user.sendRawMessage("See https://www.eclipse.org/legal/epl-2.0/");
         user.sendRawMessage("for license information.");
         return true;
