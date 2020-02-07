cf04358d14c4ea02cbc7734a94748b9e50f2bec3
tastybento <tastybento@wasteofplastic.com>
2020-02-07 13:11:54 -0800

    Use control panel if it exists.

--- src/main/java/world/bentobox/bskyblock/commands/IslandCommand.java
+++ src/main/java/world/bentobox/bskyblock/commands/IslandCommand.java
@@ -73,6 +73,9 @@ public class IslandCommand extends CompositeCommand {
         if (args.isEmpty()) {
             // If user has an island, go
             if (getPlugin().getIslands().getIsland(getWorld(), user.getUniqueId()) != null) {
+                if (getSubCommand("controlpanel").isPresent()) {
+                    return getSubCommand("controlpanel").get().call(user, label, new ArrayList<>());
+                }
                 return getSubCommand("go").map(goCmd -> goCmd.call(user, goCmd.getLabel(), new ArrayList<>())).orElse(false);
             }
             // No islands currently
