6833fb011ae56f10c3a55e30f35a6a7a9962093f
tastybento <tastybento@wasteofplastic.com>
2020-06-30 16:14:07 -0700

    Enable backwards compatibility with 1.15.2

--- src/main/java/world/bentobox/bskyblock/Settings.java
+++ src/main/java/world/bentobox/bskyblock/Settings.java
@@ -12,6 +12,8 @@ import org.bukkit.GameMode;
 import org.bukkit.block.Biome;
 import org.bukkit.entity.EntityType;
 
+import com.google.common.base.Enums;
+
 import world.bentobox.bentobox.api.configuration.ConfigComment;
 import world.bentobox.bentobox.api.configuration.ConfigEntry;
 import world.bentobox.bentobox.api.configuration.StoreAt;
@@ -148,7 +150,7 @@ public class Settings implements WorldSettings {
     private Biome defaultBiome = Biome.PLAINS;
     @ConfigComment("The default biome for the nether world (this may affect what mobs can spawn)")
     @ConfigEntry(path = "world.default-nether-biome")
-    private Biome defaultNetherBiome = Biome.NETHER_WASTES;
+    private Biome defaultNetherBiome = Enums.getIfPresent(Biome.class, "NETHER").or(Enums.getIfPresent(Biome.class, "NETHER_WASTES").or(Biome.BADLANDS));
     @ConfigComment("The default biome for the end world (this may affect what mobs can spawn)")
     @ConfigEntry(path = "world.default-end-biome")
     private Biome defaultEndBiome = Biome.THE_END;
