33a34aab44d1431a96c2613f6933f2e01ad3c8e2
tastybento <tastybento@wasteofplastic.com>
2021-05-31 16:06:52 -0700

    Rename pladdon class

--- src/main/java/world/bentobox/bskyblock/BSkyBlockPladdon.java
+++ src/main/java/world/bentobox/bskyblock/BSkyBlockPladdon.java
@@ -3,7 +3,7 @@ package world.bentobox.bskyblock;
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
-public class CopyToBentoBox extends Pladdon {
+public class BSkyBlockPladdon extends Pladdon {
 
     @Override
     public Addon getAddon() {
