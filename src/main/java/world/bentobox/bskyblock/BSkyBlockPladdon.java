0efc1af21fa50d6e1baa1f0fe4e2104a00dddae3
BONNe <bonne@bonne.id.lv>
2023-04-08 19:04:16 +0300

    Update BSkyBlockPladdon.java

--- src/main/java/world/bentobox/bskyblock/BSkyBlockPladdon.java
+++ src/main/java/world/bentobox/bskyblock/BSkyBlockPladdon.java
@@ -1,13 +1,10 @@
 package world.bentobox.bskyblock;
 
-import org.bukkit.plugin.java.annotation.plugin.ApiVersion;
-import org.bukkit.plugin.java.annotation.plugin.Plugin;
 
 import world.bentobox.bentobox.api.addons.Addon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
-@Plugin(name="BSkyBlock", version="1.0")
-@ApiVersion(ApiVersion.Target.v1_17)
+
 public class BSkyBlockPladdon extends Pladdon {
 
     @Override
