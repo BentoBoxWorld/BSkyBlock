663e8086391aec0d3f8455c88be999dd6ae35667
tastybento <tastybento@users.noreply.github.com>
2024-07-02 09:29:53 -0700

    Return the Pladdon that was generated

--- src/main/java/world/bentobox/bskyblock/BSkyBlockPladdon.java
+++ src/main/java/world/bentobox/bskyblock/BSkyBlockPladdon.java
@@ -2,13 +2,19 @@ package world.bentobox.bskyblock;
 
 
 import world.bentobox.bentobox.api.addons.Addon;
+import world.bentobox.bentobox.api.addons.GameModeAddon;
 import world.bentobox.bentobox.api.addons.Pladdon;
 
 
 public class BSkyBlockPladdon extends Pladdon {
-
+    
+    private GameModeAddon addon; 
+    
     @Override
     public Addon getAddon() {
-        return new BSkyBlock();
+        if (addon == null) {
+            addon = new BSkyBlock();
+        }
+        return addon;
     }
 }
